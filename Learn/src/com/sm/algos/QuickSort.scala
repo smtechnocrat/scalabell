1725078e1ceebf5fac4d72295621c9c77455fbe2
smtechnocrat@gmail.com <smtechnocrat@gmail.com>
2014-05-02 12:42:49 -0400

    Added tail recursive qsort.

--- Learn/src/com/sm/algos/QuickSort.scala
+++ Learn/src/com/sm/algos/QuickSort.scala
@@ -3,6 +3,8 @@
  */
 package com.sm.algos
 
+import scala.annotation.tailrec
+
 /**
  *   function quicksort('array')
  * 		if length('array') ≤ 1
@@ -24,10 +26,38 @@ object QuickSort {
   def qsort(xs: List[Int]): List[Int] = xs match {
 
     case Nil => Nil
-    case x::xst => val (before, after) = xst.partition(_< x)
-                   qsort(before) ::: x::qsort(after)
+    case x :: xst =>
+      val (before, after) = xst.partition(_ < x)
+      qsort(before) ::: x :: qsort(after)
+  }
+
+  /**
+   * *
+   * Quicksort implementation using an accumalator.
+   *
+   *
+   */
+
+  def qsort2(xs: List[Int]): List[Int] = {
+
+    @tailrec def part_acc(x: Int, xs: List[Int], l: List[Int], e: List[Int], r: List[Int], Acc: List[Int]): List[Int] = xs match {
+      case Nil => qsort_acc(l, e ::: qsort_acc(r, Acc))
+      case h :: t if (h < x) =>
+        part_acc(x, t, h :: l, e, r, Acc)
+      case h :: t if (h > x) =>
+        part_acc(x, t, l, e, h :: r, Acc)
+      case h :: t if (h == x) =>
+        part_acc(x, t, l, h :: e, r, Acc)
+
+    }
+    def qsort_acc(ls: List[Int], acc: List[Int]): List[Int] = ls match {
+      case Nil => acc
+      case (h :: t) => part_acc(h, t, List(), List(h), List(), acc)
+    }
+
+    qsort_acc(xs, List())
   }
 
   def main(args: Array[String]): Unit = {}
-   println(qsort(List(32, 12, 67, 23, 8, 1, 6, 34, 87, 24, 10, 63)))
+  println(qsort2(List(32, 12, 67, 23, 8, 1, 6, 34, 87, 24, 10, 63)))
 }
